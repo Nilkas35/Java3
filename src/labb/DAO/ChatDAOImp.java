242d18e2dc408837ba90dfad7857c9aa56353fb3
nikla <nikla@DESKTOP-76K13PL>
2021-02-24 02:32:19 +0100

    Update

--- src/labb/DAO/ChatDAOImp.java
+++ src/labb/DAO/ChatDAOImp.java
@@ -9,7 +9,6 @@ import labb.DataStructures.Message;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
